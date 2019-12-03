e52aa925c3282b0693bb71037186de515e47aec2
3Mustard <m23.cullen@gmail.com>
2019-12-03 12:18:10 -0500

    create basic new review form and create action in controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,5 +4,30 @@ class ReviewsController < ApplicationController
         @review = Review.new 
         #build book variable?  
     end 
-    
+
+    def create 
+        @review = Review.new(review_params)
+        
+        if @review.valid? 
+            @review.save 
+            redirect_to @review 
+        else 
+            render :new 
+        end 
+    end 
+
+    def show 
+        current_review
+    end 
+
+    private 
+
+    def current_review 
+        @review = Review.find(params[:id])
+    end
+
+    def review_params
+        params.require(:review).permit(:rating, :content)
+    end 
+
 end
