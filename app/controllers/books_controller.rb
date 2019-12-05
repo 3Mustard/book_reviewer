d210ec159fdbfd609ef6fa09b084b00b0a3d77f3
3Mustard <m23.cullen@gmail.com>
2019-12-04 23:30:38 -0500

    add view reviews/index and links to books/index.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
 
     def index 
+        if params[:search]
+            @book = Book.find_by(title: params[:search])
+        end 
     end 
 
     def new 
