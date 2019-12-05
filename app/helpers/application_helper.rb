367af35a7957e02acf64718236bb7c8fbc348ea6
3Mustard <m23.cullen@gmail.com>
2019-12-05 10:38:49 -0500

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,6 @@
-module ApplicationHelper
-    
+module ApplicationHelper 
+
     def current_user
         user = User.find(session[:user_id])
-    end 
-
-    def current_users_books
-        books = current_user.reviews.collect do |review|
-            books << review.book
-        end 
-    end 
-
-    
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
     #validations
     validates :username, uniqueness: true, presence: true 
 
+    #returns the users review for a given book
     def current_users_review(book_id)
         book = Book.find(book_id)
         book.reviews.find_by(user_id: current_user.id)
