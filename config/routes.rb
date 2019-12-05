a87f3df2ccee88edb0ff3970c7f7f1ca161dfbdf
3Mustard <m23.cullen@gmail.com>
2019-12-04 23:19:33 -0500

    fixed search function on books/index

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   delete '/logout' => 'sessions#destroy'
 
-  post '/search' => 'search#books'
-  
   ##resources
 
   #needed for path book/review/new or edit 
