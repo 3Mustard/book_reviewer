2f12738dce1bdb80f237f47c511ea5d6b4759645
3Mustard <m23.cullen@gmail.com>
2019-12-05 20:59:02 -0500

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user
+    helper_method :current_user 
+    helper_method :my_book
 
     def home 
     end 
@@ -9,4 +10,9 @@ class ApplicationController < ActionController::Base
     def current_user
         user = User.find(session[:user_id])
     end
+
+    #get book by reviews book_id
+    def my_book(book_id)
+        Book.find(book_id)
+    end 
 end
