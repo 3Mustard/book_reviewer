5fd385c01e0ab751323671760884e00efd76d171
3Mustard <m23.cullen@gmail.com>
2019-12-06 22:13:04 -0500

    fix issue with user model has_many

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,3 @@
 class Author < ApplicationRecord
     has_many :books
-
-    # def authors_first_and_last_name
-    #     name = self.first_name + " " + self.last_name
-    # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :reviews 
-    has_many :reviewed_books, through: :reviews
+    has_many :books, through: :reviews
     
     #validations
     validates :username, uniqueness: true, presence: true  
-
-    #create a method so calling user.books shows all the books they have reviewed
-    def books 
-        books = [] 
-        self.reviews.each do |review|
-             books << review.book 
-        end
-        books  
-    end 
 end
